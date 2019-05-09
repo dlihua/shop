5d9b986c0c84fa09188d342cace325f342e7c009
maoshu <504248911@qq.com>
2019-05-09 15:16:54 +0800

    用户及其操作方式模块

--- Mxshop/apps/user_operation/models.py
+++ Mxshop/apps/user_operation/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from goods.models import Goods
+from datetime import datetime
 
 # Create your models here.
+
+User = get_user_model()
+
+
+# 用户收藏
+class UserFav(models.Model):
+    user = models.ForeignKey(User, verbose_name='用户')
+    goods = models.ForeignKey(Goods, verbose_name='商品', help_text='商品ID')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        '''
+        unique_together：联合唯一；
+                         即条件中只要有一个不同，都会被记录为两组数据，
+                         如果全部相同，则不写入数据库中
+        '''
+        verbose_name = '用户收藏'
+        verbose_name_plural = verbose_name
+        unique_together = ('user', 'goods')
+
+    def __str__(self):
+        return self.user.name
+
+
+# 用户留言
+class UserLeavingMessage(models.Model):
+    MESSAGE_CHOICES = (
+        (1, "留言"),
+        (2, "投诉"),
+        (3, "询问"),
+        (4, "售后"),
+        (5, "求购")
+    )
+    user = models.ForeignKey(User, verbose_name='用户')
+    message_type = models.IntegerField(default=1, choices=MESSAGE_CHOICES, verbose_name='留言类型',
+                                       help_text=u'留言类型: 1(留言),2(投诉),3(询问),4(售后),5(求购)')
+    subject = models.CharField(max_length=100, default="", verbose_name="主题")
+    message = models.TextField(default="", verbose_name="留言内容", help_text="留言内容")
+    file = models.FileField(upload_to="message/images/", verbose_name="上传的文件", help_text="上传的文件")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户留言"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.subject
+
+# 用户收货地址
+class UserAddress(models.Model):
+
+    user = models.ForeignKey(User, verbose_name="用户")
+    province = models.CharField(max_length=100,default='',verbose_name="省份")
+    city = models.CharField(max_length=100,default='',verbose_name="城市")
+    district = models.CharField(max_length=100,default='',verbose_name="区域")
+    address = models.CharField(max_length=100,default='',verbose_name="详细地址")
+    signer_name = models.CharField(max_length=100,default='',verbose_name="签收人")
+    signer_mobile = models.CharField(max_length=11,default='',verbose_name="电话")
+    add_time = models.DateTimeField(default=datetime.now,verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "收货地址"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.address
--- Mxshop/apps/users/models.py
+++ Mxshop/apps/users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 # Create your models here.
+
+# 用户
+class UserProfile(AbstractUser):
+    '''
+    继承django自带的AbstractUser
+    AbstractUser：使用情景为，对django自带的User model感到满意，但想要新加几个字段
+                这时要在settings.py中添加设置
+                # settings.py
+                # 格式为 "<django_app名>.<model名>"
+                AUTH_USER_MODEL = "apps.UserPorfile"
+    '''
+
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name="姓名")
+    birthday = models.DateField(null=True, blank=True, verbose_name="出生年月")
+    gender = models.CharField(max_length=6,choices=(('male','男'),('female','女')),default='female',verbose_name='性别')
+    mobile = models.CharField(null=True, blank=True, max_length=11, verbose_name="电话")
+    email = models.EmailField(max_length=100, null=True, blank=True, verbose_name="邮箱")
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
+
+
+
+# 验证码
+class VerifyCode(models.Model):
+    """
+    短信验证码
+    """
+    code = models.CharField(max_length=10, verbose_name="验证码")
+    mobile = models.CharField(max_length=11, verbose_name="电话")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "短信验证码"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.code
+
